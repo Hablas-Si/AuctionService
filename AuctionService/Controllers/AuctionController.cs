48034afd3fc7a107f698de0c5f7f6c47bc11f8a6
DenGuleDansker <chilinhm@gmail.com>
2024-05-27 11:46:27 +0200

    made a getall auctions

--- AuctionService/Controllers/AuctionController.cs
+++ AuctionService/Controllers/AuctionController.cs
@@ -34,7 +34,7 @@ namespace AuctionService.Controllers
         public async Task<IActionResult> getAll()
         {
             _logger.LogInformation("Fetching all items from catalog");
-            var auctionList = await _auctionService.getAll();
+            var auctionList = await _auctionService.GetAllAuctions();
             return Ok(auctionList);
         }
 
