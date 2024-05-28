ab6b7d8b89cc84a46d488279b083fee3e85a6fcb
FynboNohr69420 <117977003+FynboNohr69420@users.noreply.github.com>
2024-05-28 10:22:22 +0200

    Adding missing singleton

--- AuctionService/Program.cs
+++ AuctionService/Program.cs
@@ -98,6 +98,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddSingleton<RabbitMQSubscriber>();
+
 var app = builder.Build();
 
 // Start the RabbitMQ listener
